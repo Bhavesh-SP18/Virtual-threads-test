94a590082becfa920a8004be9d3298f605c16199
bhavesh <bhavesh.kadam@sparkeighteen.com>
2022-11-30 00:03:01 +0530

    try with virtual

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,10 @@ import java.util.concurrent.Executors;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         VirtualThreds virtualThreds = new VirtualThreds();
-        ExecutorService executor = Executors.newFixedThreadPool(400);
-//        ExecutorService executorVirtual = Executors.newVirtualThreadPerTaskExecutor();
+//        ExecutorService executor = Executors.newFixedThreadPool(800);
+        ExecutorService executorVirtual = Executors.newVirtualThreadPerTaskExecutor();
         Instant start = Instant.now();
-        virtualThreds.executeTask(1000, executor);
+        virtualThreds.executeTask(1000, executorVirtual);
         Instant end = Instant.now();
         long seconds = ChronoUnit.SECONDS.between(start, end);
         System.out.println(String.format("Done in %d seconds", seconds));
